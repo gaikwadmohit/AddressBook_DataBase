ea56ef41344d2db2121ce1ced21296c0732c9e7f
MOHIT BHASKARRAO GAIKWAD <97322025+gaikwadmohit@users.noreply.github.com>
2022-05-07 10:55:28 +0530

    Ability to create a Address Book table with  first ,last, address,city,state etc

--- addressbook_database.sql
+++ addressbook_database.sql
@@ -2,3 +2,18 @@
 create database AddressBook_services;
 show databases;
 use AddressBook_services;
+
+#UC2
+create table Address_book(
+	id int unsigned not null auto_increment,
+	fname varchar(15) not null,
+    lname varchar(10) not null,
+    city varchar(50) not null,
+    state varchar(50) not null,
+    zip int not null,
+    phone long not null,
+    email varchar(100) not null,
+     primary key (id)
+);
+select * from address_book;
+
